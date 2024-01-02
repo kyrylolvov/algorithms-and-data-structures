6a1d8f69ea1f05a809142efb52773cede69086aa
kyrylolvov <kyrylolvov@icloud.com>
2024-01-02 09:24:30 -0500

    CHANGED: - Finished "Filter Elements from Array" task

---
+++ 30-days-with-js/filter.ts
@@ -0,0 +1,17 @@
+type Fn = (n: number, i: number) => any;
+
+function filter(arr: number[], fn: Fn): number[] {
+  let result: number[] = [];
+
+  for (let i = 0; i <= arr.length; i++) {
+    if (fn(arr[i], i)) result.push(arr[i]);
+  }
+
+  return result;
+}
+
+console.log(
+  filter([0, 10, 20, 30], function greaterThan10(n) {
+    return n > 10;
+  }),
+);
