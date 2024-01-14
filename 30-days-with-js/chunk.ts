56ff7a5a888025268f7102025e175f6ae3554a41
kyrylolvov <kyrylolvov@icloud.com>
2024-01-14 08:09:42 -0500

    CHANGED: - Finished "Chunk Array" task from 30 Days of JavaScript

---
+++ 30-days-with-js/chunk.ts
@@ -0,0 +1,17 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+  const chunkedArray: Obj[][] = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    chunkedArray.push(arr.slice(i, i + size));
+  }
+
+  return chunkedArray;
+}
+
+console.log(chunk([1, 2, 3, 4, 5], 1));
+console.log(chunk([1, 9, 6, 3, 2], 3));
+console.log(chunk([[8, 5, 3, 2, 6]], 6));
+console.log(chunk([], 6));
